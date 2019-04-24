afbe7487620062a12477c6c4f9b01e249ac773b0
Naresh8460 <mudaliar.naresh@gmail.com>
2019-04-24 17:36:22 +0530

    -A

---
+++ Udyog/routes/Profile/viewProfile.js
@@ -0,0 +1,31 @@
+'use strict'
+
+var getData = function(request, response) {
+    if(!request.session.user) {
+        return response.redirect("/recruiterLogin");
+    }
+
+    var DB = request.app.locals.DB;
+
+    var studentId = request.params.studentId;
+
+    DB.collection("student").findOne({_id: mongo.ObjectId(studentId)}, function(error, allPost) {
+      
+		if(error) {
+			return response.send("error fetching job from the DB");
+		}
+
+        var data = {
+            allPost: allPost
+        };
+   
+         return response.render("student-apply.hbs", data);
+    });
+    
+}
+
+
+
+
+
+exports.getData = getData;
