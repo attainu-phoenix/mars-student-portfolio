3b8a3bd28ee19dc6c817675684ce3e6708b6f789
Srinivas-Namballa <namballasrinivas902@gmail.com>
2019-05-05 19:59:42 +0530

    Updated student dashboard

--- routes/Dashboard/studentDashboard.js
+++ routes/Dashboard/studentDashboard.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var mongo = require("mongodb");
+
 var getData = function(request, response) {
     if(!request.session.user) {
         return response.redirect("/");
@@ -8,14 +10,35 @@ var getData = function(request, response) {
     var DB = request.app.locals.DB;
 
     var querysearch = request.query.searchJobs;
+
+    var data = {};
+    var studentId;
+
+    studentId = request.session.user._id;
+
+    DB.collection("students").findOne({ _id: mongo.ObjectID(studentId) }, function (error, student) {
+        if (error) { return response.send("error fetching user data"); }
+        data.student = student;
+        // return response.render("studentsDashboard.hbs", data);
+    });
     
     DB.collection("recruiterPostJobs").find({keySkills: {$regex: querysearch}}).toArray(function(error, jobs) { 
-        var data = {
-            jobs: jobs
-        }
-
-        return response.render("studentsDashboard.hbs", data);
+        data.jobs = jobs;
+        response.render("studentsDashboard.hbs", data);
     });
+    
 };
 
+// var getImgData = function (request, response) {
+//     if (!request.session.user) {
+//         return response.redirect("/");
+//     }
+
+//     var DB = request.app.locals.DB;
+//     //console.log(request.session.user);
+
+    
+// }
+
 exports.getData = getData;
+// exports.getImgData = getImgData;
